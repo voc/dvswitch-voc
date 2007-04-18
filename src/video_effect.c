70078cc10a7c102fbe0b44e515e6ab365175aa8e
Ben Hutchings <ben@decadent.org.uk>
2007-04-18 18:24:00 +0000

    Rewrote video_effect_pic_in_pic to *downscale* correctly.

--- src/video_effect.c
+++ src/video_effect.c
@@ -1,8 +1,8 @@
 // Copyright 2007 Ben Hutchings and Tore Sinding Bekkedal.
-// Scaling code is based on SDL_rotozoom.c; copyright A Schiffler.
 // See the file "COPYING" for licence details.
 
 #include <assert.h>
+#include <string.h>
 
 #include "video_effect.h"
 
@@ -54,131 +54,155 @@ void video_effect_pic_in_pic(struct frame_decoded_ref dest,
 
     assert(left < right && right <= FRAME_WIDTH);
     assert(top < bottom && bottom <= dest.height);
+    assert(right - left < FRAME_WIDTH);
+    assert(bottom - top < dest.height);
 
-    unsigned x, y, sx, sy, *csaxy, *csaxc, *csay, cs, exy, exc, ey, t1, t2, sstep;
-    uint8_t *c0y, *c0c, *c1y, *c1c;
-    uint8_t *sp, *csp, *dp;
-    unsigned dgap;
+    // Scaling tables
 
-    /*
-     * For interpolation: assume source dimension is one pixel 
-     */
-    /*
-     * smaller to avoid overflow on right and bottom edge.     
-     */
-    sx = (unsigned) (65536.0 * (float) (FRAME_WIDTH - 1) / (float) (right - left));
-    sy = (unsigned) (65536.0 * (float) (source.height - 1) / (float) (bottom - top));
+    struct weights {
+	// Weight of source column/row on current dest column/row
+	uint16_t cur;
+	// Weight of source column/row on next dest column/row, plus 1
+	// if this the last source column/row for this dest column/row.
+	uint16_t spill;
+    };
+    struct weights y_col_weights[FRAME_WIDTH];
+    struct weights c_col_weights[FRAME_WIDTH / 2];
+    struct weights row_weights[FRAME_HEIGHT_MAX];
+    unsigned e, x, y;
 
-    /*
-     * Precalculate row increments 
-     */
-    unsigned saxy[FRAME_WIDTH + 1];
-    unsigned saxc[FRAME_WIDTH / 2 + 1];
-    unsigned say[FRAME_HEIGHT_MAX + 1];
-
-    cs = 0;
-    csaxy = saxy;
-    for (x = left; x <= right; x++) {
-	*csaxy = cs;
-	csaxy++;
-	cs &= 0xffff;
-	cs += sx;
-    }
-    cs = 0;
-    csaxc = saxc;
-    for (x = left; x <= right; x += 2) {
-	*csaxc = cs;
-	csaxc++;
-	cs &= 0xffff;
-	cs += sx;
-    }
-    cs = 0;
-    csay = say;
-    for (y = top; y <= bottom; y++) {
-	*csay = cs;
-	csay++;
-	cs &= 0xffff;
-	cs += sy;
-    }
-
-    sp = csp = source.pixels;
-    dp = dest.pixels + top * dest.pitch + left * FRAME_BYTES_PER_PIXEL;
-
-    dgap = dest.pitch - (right - left) * FRAME_BYTES_PER_PIXEL;
-
-    /*
-     * Scan destination 
-     */
-    csay = say;
-    for (y = top; y < bottom; y++) {
-	ey = (*csay & 0xffff);
-
-	/*
-	 * Setup color source pointers 
-	 */
-	c0y = csp;
-	c0c = c0y;
-	c1y = csp + source.pitch;
-	c1c = c1y;
-	csaxy = saxy;
-	csaxc = saxc;
-	for (x = left; x < right; x += 2) {
-
-	    /*
-	     * Interpolate Y', Cb
-	     */
-	    exy = (*csaxy & 0xffff);
-	    exc = (*csaxc & 0xffff);
-	    t1 = ((((c0y[2] - c0y[0]) * exy) >> 16) + c0y[0]) & 0xff;
-	    t2 = ((((c1y[2] - c1y[0]) * exy) >> 16) + c1y[0]) & 0xff;
-	    dp[0] = (((t2 - t1) * ey) >> 16) + t1;
-	    t1 = ((((c0c[5] - c0c[1]) * exc) >> 16) + c0c[1]) & 0xff;
-	    t2 = ((((c1c[5] - c1c[1]) * exc) >> 16) + c1c[1]) & 0xff;
-	    dp[1] = (((t2 - t1) * ey) >> 16) + t1;
-
-	    /*
-	     * Advance Y' source pointer
-	     */
-	    csaxy++;
-	    sstep = (*csaxy >> 16) * 2;
-	    c0y += sstep;
-	    c1y += sstep;
-
-	    /*
-	     * Interpolate Y', Cr
-	     */
-	    exy = (*csaxy & 0xffff);
-	    t1 = ((((c0y[2] - c0y[0]) * exy) >> 16) + c0y[0]) & 0xff;
-	    t2 = ((((c1y[2] - c1y[0]) * exy) >> 16) + c1y[0]) & 0xff;
-	    dp[2] = (((t2 - t1) * ey) >> 16) + t1;
-	    t1 = ((((c0c[7] - c0c[3]) * exc) >> 16) + c0c[3]) & 0xff;
-	    t2 = ((((c1c[7] - c1c[3]) * exc) >> 16) + c1c[3]) & 0xff;
-	    dp[3] = (((t2 - t1) * ey) >> 16) + t1;
-
-	    /*
-	     * Advance source pointers 
-	     */
-	    csaxy++;
-	    sstep = (*csaxy >> 16) * 2;
-	    c0y += sstep;
-	    c1y += sstep;
-	    csaxc++;
-	    sstep = (*csaxc >> 16) * 4;
-	    c0c += sstep;
-	    c1c += sstep;
-	    /*
-	     * Advance destination pointer 
-	     */
-	    dp += 4;
+    e = 0;
+    for (x = 0; x != FRAME_WIDTH; ++x)
+    {
+	e += right - left;
+	y_col_weights[x].cur = ((right - left) << 16) / FRAME_WIDTH;
+	if (e >= FRAME_WIDTH)
+	{
+	    e -= FRAME_WIDTH;
+	    unsigned next_weight = (e << 16) / FRAME_WIDTH;
+	    y_col_weights[x].spill = 1 + next_weight;
+	    y_col_weights[x].cur -= next_weight;
 	}
-	/*
-	 * Advance source pointer 
-	 */
-	csay++;
-	csp += (*csay >> 16) * source.pitch;
-	/*
-	 * Advance destination pointers 
-	 */
-	dp += dgap;
+	else
+	{
+	    y_col_weights[x].spill = 0;
+	}
     }
+
+    e = 0;
+    for (x = 0; x != FRAME_WIDTH / 2; ++x)
+    {
+	e += right - left;
+	c_col_weights[x].cur = ((right - left) << 16) / FRAME_WIDTH;
+	if (e >= FRAME_WIDTH)
+	{
+	    e -= FRAME_WIDTH;
+	    unsigned next_weight = (e << 16) / FRAME_WIDTH;
+	    c_col_weights[x].spill = 1 + next_weight;
+	    c_col_weights[x].cur -= next_weight;
+	}
+	else
+	{
+	    c_col_weights[x].spill = 0;
+	}
+    }
+
+    e = 0;
+    for (y = 0; y != source.height; ++y)
+    {
+	e += bottom - top;
+	row_weights[y].cur = ((bottom - top) << 16) / source.height;
+	if (e >= source.height)
+	{
+	    e -= source.height;
+	    unsigned next_weight = (e << 16) / source.height;
+	    row_weights[y].spill = 1 + next_weight;
+	    row_weights[y].cur -= next_weight;
+	}
+	else
+	{
+	    row_weights[y].spill = 0;
+	}
+    }
+
+    assert(y_col_weights[FRAME_WIDTH - 1].spill == 1);
+    assert(c_col_weights[FRAME_WIDTH / 2 - 1].spill == 1);
+    assert(row_weights[source.height - 1].spill == 1);
+
+    uint8_t * dp =
+	dest.pixels + top * dest.pitch + left * FRAME_BYTES_PER_PIXEL;
+
+    unsigned dest_gap = dest.pitch - (right - left) * FRAME_BYTES_PER_PIXEL;
+
+    uint32_t y_row_buffer[FRAME_WIDTH], c_row_buffer[FRAME_WIDTH], *yp, *cp;
+    memset(y_row_buffer, 0, (right - left) * sizeof(uint32_t));
+    memset(c_row_buffer, 0, (right - left) * sizeof(uint32_t));
+
+    // Loop over source rows
+    for (y = 0; y != source.height; ++y)
+    {
+	const uint8_t * sp = source.pixels + y * source.pitch;
+	unsigned row_weight = row_weights[y].cur;
+	unsigned row_spill = row_weights[y].spill;
+
+	for (;;)
+	{
+	    // Loop over source columns
+	    yp = y_row_buffer;
+	    cp = c_row_buffer;
+	    for (x = 0; x != FRAME_WIDTH; x += 2)
+	    {
+		// Process two Y' values
+		*yp += (row_weight * y_col_weights[x].cur >> 8) * sp[0];
+		if (y_col_weights[x].spill)
+		    *++yp += ((row_weight * (y_col_weights[x].spill - 1) >> 8)
+			      * sp[0]);
+		*yp += (row_weight * y_col_weights[x + 1].cur >> 8) * sp[2];
+		if (y_col_weights[x + 1].spill)
+		    *++yp += ((row_weight * (y_col_weights[x + 1].spill - 1)
+			       >> 8)
+			      * sp[2]);
+		// Then the Cb and Cr values
+		unsigned c_col_weight = c_col_weights[x / 2].cur;
+		cp[0] += (row_weight * c_col_weight >> 8) * sp[1];
+		cp[1] += (row_weight * c_col_weight >> 8) * sp[3];
+		if (c_col_weights[x / 2].spill)
+		{
+		    cp += 2;
+		    c_col_weight = c_col_weights[x / 2].spill - 1;
+		    cp[0] += (row_weight * c_col_weight >> 8) * sp[1];
+		    cp[1] += (row_weight * c_col_weight >> 8) * sp[3];
+		}
+		sp += 4;
+	    }
+
+	    if (!row_spill)
+		break;
+
+	    // Spit out destination row, interleaving Y' and Cb/Cr again
+	    yp = y_row_buffer;
+	    cp = c_row_buffer;
+	    for (x = left; x != right; ++x)
+	    {
+		*dp++ = *yp++ >> 24;
+		*dp++ = *cp++ >> 24;
+	    }
+
+	    if (y == source.height - 1)
+		break;
+
+	    dp += dest_gap;
+	    memset(y_row_buffer, 0, (right - left) * sizeof(uint32_t));
+	    memset(c_row_buffer, 0, (right - left) * sizeof(uint32_t));
+
+	    // Scale source row to next dest row if it overlaps
+	    row_weight = row_spill - 1;
+	    row_spill = 0;
+	    if (!row_weight)
+		break;
+	}
+    }
+
+    assert(dp == (dest.pixels + (bottom - 1) * dest.pitch
+		  + right * FRAME_BYTES_PER_PIXEL));
 }
